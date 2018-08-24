dcb9f5a619e4d7f10e5d3164a3646b5cf658104d
Erik <erikaanestad@gmail.com>
2018-08-23 21:38:49 -0700

    no errors.... no results.... lets try no cors?

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ const cors = require('cors');
 //   res.header('Access-Control-Allow-Headers', 'Content-Type');
 //   next();
 // });
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 app.use(express.json());
 // app.use(express.static(path.join(__dirname, 'public')));
 
