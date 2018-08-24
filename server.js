6e0723617efce9ce6e33fdab73bf518b31f53534
Erik <erikaanestad@gmail.com>
2018-08-23 21:27:01 -0700

    'more CORS'

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ var serveStatic = require('serve-static');
 app.use(serveStatic(__dirname + "/dist"));
 
 // middleware (cors and read json body)
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const cors = require('cors');
 // const morgan = require('morgan');
 // app.use(morgan('dev'));
