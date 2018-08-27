b4d9d1e3ff99c64acbf8cb444347ca21bca35cc9
Erik <erikaanestad@gmail.com>
2018-08-26 22:52:50 -0700

    ''

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
+const PORT = '';
 console.log('process.env.PORT=', process.env.PORT);
 // const URL = `http://localhost:${PORT}/api`;
 const URL = `https://pick-a-spot.herokuapp.com:${PORT}/api`;
